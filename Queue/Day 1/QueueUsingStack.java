79d40da5bf5e07a8eaca03af266d77a64a0c508b
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-21 22:03:18 +0530

    queue using two stacks

---
+++ Queue/Day 1/QueueUsingStack.java
@@ -0,0 +1,60 @@
+import java.util.Stack;
+
+public class QueueUsingStack {
+    static class Queue{
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        // isEmpty
+        boolean isEmpty(){
+            return s1.isEmpty();
+        }
+
+        // add -> O(n)
+        void add(int data){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+
+            s1.push(data);
+
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+        }
+
+        //remove
+        int remove(){
+            if(s1.isEmpty()){
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+
+            return s1.pop();
+        }
+
+        // peek
+        int peek(){
+            if(s1.isEmpty()){
+                System.out.println("Queue is Empty");
+                return -1;
+            }
+
+            return s1.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        q.remove();
+
+        while(!q.isEmpty()){
+            System.out.print(q.peek()+" ");
+            q.remove();
+        }
+    }
+}
