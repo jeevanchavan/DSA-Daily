1ce137304b84f9c98d491ebd6ca1f99e61e59c6a
Jeevan Chavan <jeevanchavan7273@gmail.com>
2025-12-23 21:41:54 +0530

    same logical problem on activity selection

---
+++ Greedy/classic problems/NonOverlappingIntervals.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class NonOverlappingIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = {{1,2},{2,3},{3,4},{1,3}};
+
+        // Sort intervals by end time
+        // TC for sort: O(n log n)
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+
+        int remove = 0;              // count of removed intervals
+        int lastEnd = intervals[0][1]; // end time of last selected interval
+
+        // Start from index 1 (first interval already selected)
+        for(int i = 1; i < intervals.length; i++){
+            // If current interval overlaps with previous one
+            if(intervals[i][0] < lastEnd){
+                remove++; // remove current interval
+            } else {
+                // No overlap, update lastEnd
+                lastEnd = intervals[i][1];
+            }
+        }
+
+        // Final answer
+        System.out.println("Number of removed intervals: " + remove);
+    }
+}
